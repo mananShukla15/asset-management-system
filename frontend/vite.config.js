8ee9e05230a75824ace0cbf9db89aeb68a6a727a
shuklamanan <mananshukla1512@gmail.com>
2025-03-22 22:06:41 +0530

    Feat: Refactor
    Took 1 hour 24 minutes

---
+++ frontend/vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    base: './',
+    publicDir: "public",  // Keeps static assets accessible
+    build: {
+        outDir: 'dist',   // Vercel will serve from here
+        target: 'esnext'
+    }
+});
